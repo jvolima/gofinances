1e5fe25775b37b24bf39e16d7cd84c2ca0e72813
João Vitor Lima <jvolima2004@gmail.com>
2022-05-19 18:51:43 -0300

    componente HighlightCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
 import { 
   Container,
   Header,
@@ -8,7 +9,8 @@ import {
   User,
   UserGreeting,
   UserName,
-  Icon
+  Icon,
+  HighlightCards
 } from './styles';
 
 export function Dashboard() {
@@ -28,6 +30,26 @@ export function Dashboard() {
         </UserWrapper>
       </Header>
 
+      <HighlightCards>
+        <HighlightCard 
+          type="up"
+          title="Entradas" 
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard 
+          type="down"
+          title="Saídas" 
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard 
+          type="total"
+          title="Total" 
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper"
 
 export const Container = styled.View`
   flex: 1;
@@ -10,16 +11,19 @@ export const Container = styled.View`
 export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(42)}px;
+
   background-color: ${({ theme }) => theme.colors.primary};
 
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  flex-direction: row;
 `;  
 
 export const UserWrapper = styled.View`
   width: 100%;
 
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
 
   flex-direction: row;
   justify-content: space-between;
@@ -45,18 +49,28 @@ export const User = styled.View`
 export const UserGreeting = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
 
-  font-size: ${RFValue(18)};
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.regular};
 `;
 
 export const UserName = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
   
-  font-size: ${RFValue(18)};
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
 
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 }
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
 `;
