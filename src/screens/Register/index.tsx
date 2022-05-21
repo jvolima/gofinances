a10d9ecce91214aacf3f53b5c61ef9582a1b5e9f
João Vitor Lima <jvolima2004@gmail.com>
2022-05-21 19:46:52 -0300

    Componente TransactionTypeButton e CategorySelect

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../components/Form/Button";
+import { CategorySelect } from "../../components/Form/CategorySelect";
 import { Input } from "../../components/Form/Input/index"
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 
 import { 
   Container,
   Header,
   Title,
   Form,
-  Fields
+  Fields,
+  TransactionsTypes
 } from "./styles";
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState('');
+  
+  function handleTransactionTypeSelect(type: 'up' | 'down') {
+    setTransactionType(type);
+  }
+
   return (
     <Container>
       <Header>
@@ -25,6 +34,23 @@ export function Register() {
           <Input
             placeholder="Preço"
           />
+
+          <TransactionsTypes>
+            <TransactionTypeButton 
+              title="Income"
+              type="up"
+              onPress={() => handleTransactionTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton 
+              title="Outcome"
+              type="down"
+              onPress={() => handleTransactionTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionsTypes>
+
+          <CategorySelect title="Categoria" />
         </Fields>
 
         <Button title="Enviar"/>
