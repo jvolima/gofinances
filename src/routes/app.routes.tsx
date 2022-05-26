bf1cbf280986ae907be01373e01b0c39f081db78
João Vitor Lima <jvolima2004@gmail.com>
2022-05-26 11:33:03 -0300

    Listagem de gasto por categoria

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
 import { useTheme } from "styled-components";
 import { Platform } from "react-native";
+import { Resume } from "../screens/Resume";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -52,7 +53,7 @@ export function AppRoutes() {
       />
       <Screen 
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: (({ size, color }) => 
             <MaterialIcons
