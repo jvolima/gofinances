e71a6f4fc1321bdb52b1b19b93dd3029a34a2c27
João Vitor Lima <jvolima2004@gmail.com>
2022-05-30 18:30:44 -0300

    começando criação da tela de signIn

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 import { StatusBar } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,7 +33,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
-        <AppRoutes />   
+        <SignIn />   
       </NavigationContainer>      
     </ThemeProvider>
   )
